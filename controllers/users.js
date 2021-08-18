a2b83cdf26e4718375707089a3b702d162af21ed
Narasimha Kamath <narasimha.kamath@mobisy.com>
2021-08-18 21:41:36 +0530

    Implemented async handler in users controller.

--- controllers/users.js
+++ controllers/users.js
@@ -1,52 +1,42 @@
 const User = require("./../models/User");
+const ErrorResponse = require("./../utils/errorResponse");
+const asyncHandler = require("./../middlewares/async");
 
-exports.getUsers = async (request, response, next) => {
-	try {
-		const usersData = await User.find();
-		response.status(200).json({success: true, message: "Resource found.", data: usersData});
-	} catch(error) {
-		response.status(400).json({success: false, message: "Internal server error."});
-	}
-};
+exports.getUsers = asyncHandler(async (request, response, next) => {
+	const usersData = await User.find();
 
-exports.getUser = async (request, response, next) => {
-	try {
-		const userData = await User.findById(request['params']['id']);
-		if(!userData)
-			response.status(404).json({success: false, message: "Resource not found."});
-		response.status(200).json({success: true, message: "Resource found.", data: userData});
-	} catch(error) {
-		response.status(400).json({success: true, message: "Internal server error."});
-	}
-};
+	response.status(200).json({success: true, message: "Resources found.", data: usersData});
+});
 
-exports.createUser = async (request, response, next) => {
-	try {
-		const user = await User.create(request['body']);
-		response.status(201).json({success: true, message: "Resource created.", data: user});
-	} catch(error) {
-		response.status(400).json({success: false, message: "Internal server error."});
-	}
-};
+exports.getUser = asyncHandler(async (request, response, next) => {
+	const userData = await User.findById(request['params']['id']);
 
-exports.updateUser = async (request, response, next) => {
-	try {
-		const userData = await User.findByIdAndUpdate(request['params']['id'], request['body'], {new: true, runValidators: true});
-		if(!userData)
-			response.status(404).json({success: false, message: "Resource not found."});
-		response.status(200).json({success: true, message: "Resource updated.", data: userData});
-	} catch(error) {
-		response.status(400).json({success: false, message: "Internal server error."});
-	}
-};
+	if(!userData)
+		return new ErrorResponse(`User not found with ID: ${request['params']['id']}`, 404);
 
-exports.deleteUser = async (request, response, next) => {
-	try {
-		const userData = await User.findByIdAndDelete(request['params']['id']);
-		if(!userData)
-			response.status(404).json({success: false, message: "Resource not found."});
-		response.status(200).json({success: true, message: "Resource deleted."});
-	} catch(error) {
-		response.status(400).json({success: false, message: "Internal server error."});
-	}
-};
+	response.status(200).json({success: true, message: "Resource found.", data: userData});
+});
+
+exports.createUser = asyncHandler(async (request, response, next) => {
+	const user = await User.create(request['body']);
+
+	response.status(201).json({success: true, message: "Resource created.", data: user});
+});
+
+exports.updateUser = asyncHandler(async (request, response, next) => {
+	const userData = await User.findByIdAndUpdate(request['params']['id'], request['body'], {new: true, runValidators: true});
+
+	if(!userData)
+		return new ErrorResponse(`User not found with ID: ${request['params']['id']}`, 404);
+
+	response.status(200).json({success: true, message: "Resource updated.", data: userData});
+});
+
+exports.deleteUser = asyncHandler(async (request, response, next) => {
+	const userData = await User.findByIdAndDelete(request['params']['id']);
+
+	if(!userData)
+		return new ErrorResponse(`User not found with ID: ${request['params']['id']}`, 404);
+
+	response.status(200).json({success: true, message: "Resource deleted."});
+});
